e60b60053dc9ece48c0a3457468a3d6b3859bfd6
denisskripnik <valis.source@gmail.com>
2020-11-10 23:08:50 +0200

    3

--- init-database.sh
+++ init-database.sh
@@ -3,7 +3,7 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 
-CREATE USER faceapp;
+CREATE USER faceapp WITH PASSWORD 'secretPassword';
 
 CREATE DATABASE faceapp;
 GRANT ALL PRIVILEGES ON DATABASE faceapp TO faceapp;
