a7658774115d26daf2b5110a5277d4c4b9fa2577
denisskripnik <valis.source@gmail.com>
2020-11-10 16:23:59 +0200

    docker compose

--- init-database.sh
+++ init-database.sh
@@ -3,7 +3,7 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 
-CREATE USER faceapp WITH PASSWORD=f1123app!22;
+CREATE USER faceapp;
 
 CREATE DATABASE faceapp;
 GRANT ALL PRIVILEGES ON DATABASE faceapp TO faceapp;
